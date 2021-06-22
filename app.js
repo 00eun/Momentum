a2c4c8097475c6107f3cc4859cb6fad5637a7da8
youngeun <00eun1128@gmail.com>
2021-06-22 23:02:44 +0900

    변수

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "youngeun";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "kim young eun";
+console.log("your new name is " + myName);
+
+const amIfat = true;
+console.log(amIfat);
