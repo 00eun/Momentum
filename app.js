3d0da5bb93929fe95b29fad1ce71a453c7f2ff28
youngeun <00eun1128@gmail.com>
2021-06-24 14:58:13 +0900

    function

--- app.js
+++ app.js
@@ -20,3 +20,28 @@
 // console.log(player);
 // player.points = player.points + 15;
 // console.log(player);
+
+//반복해서 사용할 수 있는 코드조각
+// function
+
+// function sayHello(userName, age) {
+//   console.log("Hello my name is " + userName + " and I'm " + age);
+// }
+// sayHello("ye", 10);
+
+// function divide(a, b) {
+//   console.log(a / b);
+// }
+
+// divide(10, 2);
+
+const player = {
+  name: "ye",
+  sayHello: function (otherPersonsName) {
+    console.log("Hello " + otherPersonsName + " nice to meet you!");
+  },
+};
+
+player.sayHello("eun");
+player.sayHello("nico");
+player.sayHello("console");
