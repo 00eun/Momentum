7aeac6fb3e21ce4e5e6e195b58489f631132cc4a
youngeun <00eun1128@gmail.com>
2021-06-24 14:20:46 +0900

    object

--- app.js
+++ app.js
@@ -6,17 +6,17 @@
 // console.log(daysOfWeek);
 
 //Object
-const player = {
-  name: "ye",
-  points: 10,
-  fat: true,
-};
-console.log(player);
-console.log(player.name);
+// const player = {
+//   name: "ye",
+//   points: 10,
+//   fat: true,
+// };
+// console.log(player);
+// console.log(player.name);
 
-player.fat = false;
-console.log(player);
-player.lastName = "potato";
-console.log(player);
-player.points = player.points + 15;
-console.log(player);
+// player.fat = false;
+// console.log(player);
+// player.lastName = "potato";
+// console.log(player);
+// player.points = player.points + 15;
+// console.log(player);
