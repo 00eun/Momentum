bece1e3fd4cb4c4a338cfcd07bfe7e5fd3f744c2
youngeun <00eun1128@gmail.com>
2021-06-24 14:20:30 +0900

    object

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-console.log(daysOfWeek);
+// console.log(daysOfWeek);
 
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
+// daysOfWeek.push("sun");
+// console.log(daysOfWeek);
+
+//Object
+const player = {
+  name: "ye",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+console.log(player.name);
+
+player.fat = false;
+console.log(player);
+player.lastName = "potato";
+console.log(player);
+player.points = player.points + 15;
+console.log(player);
