1088ef98b6ee136a642cd68937017252b6acf7d6
youngeun <00eun1128@gmail.com>
2021-06-27 11:57:42 +0900

    로그인전 CSS수정

--- js/backgruond.js
+++ js/backgruond.js
@@ -3,5 +3,7 @@ const bgImg = ["0.jpg", "1.jpg", "2.jpg"];
 const todaysImg = bgImg[Math.floor(Math.random() * bgImg.length)];
 
 const newImg = document.createElement("img");
-newImg.src = `img/${todaysImg}`;
+//newImg.src = `img/${todaysImg}`;
 document.body.appendChild(newImg);
+
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(img/${todaysImg})`;
