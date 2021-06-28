eb8806f6b909315c265c175a5f836bdebe958347
youngeun <00eun1128@gmail.com>
2021-06-28 12:22:56 +0900

    변수수정

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   // console.log(lat, lon);
-  const API_KEY = confing.API_KEY;
+  const API_KEY = config.API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
